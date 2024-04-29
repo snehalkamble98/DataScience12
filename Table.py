90923b8804d7599fa1ad19232e5e00da680c1b3d
snehalkamble98 <168407202+snehalkamble98@users.noreply.github.com>
2024-04-29 13:13:27 +0530

    Table.py

---
+++ Table.py
@@ -0,0 +1,27 @@
+i=1
+print("**** 2's Table ****")
+while i<=10:
+    print(i*2)
+    i=i+1
+print("2's table completed\n")
+
+i=1
+print("**** 3's Table ****")
+while i<=10:
+    print(i*3)
+    i=i+1
+print("3's table completed\n")
+
+i=1
+print("**** 4's Table ****")
+while i<=10:
+    print(i*4)
+    i=i+1
+print("4's table completed\n")
+
+i=1
+print("**** 5's Table ****")
+while i<=10:
+    print(i*5)
+    i=i+1
+print("5's table completed\n")
