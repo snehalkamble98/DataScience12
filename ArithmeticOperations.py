4e122cc47481542575fc9cda1e73092c790641a3
snehalkamble98 <168407202+snehalkamble98@users.noreply.github.com>
2024-04-29 12:55:42 +0530

    ArithmeticOperations.py

---
+++ ArithmeticOperations.py
@@ -0,0 +1,22 @@
+ #### Arithmetic Operations ####
+
+
+	first_variable=int(input("Enter the first number: "))
+
+	second_variable=int(input("Enter the second number: "))
+
+	addition=first_variable+second_variable
+
+	subtraction=first_variable-second_variable
+
+	multiplication=first_variable*second_variable
+
+	devision=int(first_variable/second_variable)
+
+	print(addition)
+
+	print(subtraction)
+
+	print(multiplication)
+
+	print(devision)
