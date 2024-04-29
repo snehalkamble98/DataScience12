7be08f77fb536817f10c6493b65aa40a04153653
snehalkamble98 <168407202+snehalkamble98@users.noreply.github.com>
2024-04-29 13:03:33 +0530

    if-elif.py

---
+++ if-elif.py
@@ -0,0 +1,11 @@
+branch=input("Enter your branch: ")
+if branch=="CSE":
+    print("Exam is started from 12th June")
+elif branch=="IT":
+    print("Exam is started from 13th June")
+elif branch=="Mechanical":
+    print("Exam is started from 14th June")
+elif branch=="ENTC":
+  print("Exam is started from 15th June")
+else:
+    print("Invalid Input")
