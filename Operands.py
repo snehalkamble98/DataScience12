4c577267e0d362b6d6e8bf220d5b7c35a2f4bbb9
snehalkamble98 <168407202+snehalkamble98@users.noreply.github.com>
2024-04-29 12:58:22 +0530

    Operands.py

---
+++ Operands.py
@@ -0,0 +1,38 @@
+#### Operands ####
+
+
+first_variable = int(input("Enter the first number: "))
+
+	second_variable = int(input("Enter the second number: "))
+
+	operand = input("Enter the operand: ")
+
+	result = 0
+
+
+	if operand == "+":
+
+	result = first_variable + second_variable
+
+	print(result)
+
+
+	if operand == "-":
+
+	result = first_variable - second_variable
+
+	print(result)
+
+
+	if operand == "*":
+
+	result = first_variable * second_variable
+
+	print(result)
+
+
+	if operand == "/":
+
+	result = first_variable / second_variable
+
+	 print(result)
